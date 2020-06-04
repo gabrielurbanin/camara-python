a25e62a70c3a52d1c10f44055b8b1a317aaf81a8
Gabriel Carneiro <gabriel.carneiro97@live.com>
2020-06-04 17:10:26 -0300

    comecei o serviço de acesso aos arquivos

---
+++ src/file.service.py
@@ -0,0 +1,23 @@
+from memoization import cached
+from os import listdir
+
+def set_data_folder(dir_string = "./", tries_count = 0):
+  dir = listdir(dir_string)
+
+  if tries_count == 3:
+    raise FileNotFoundError("Você está executando esse código em um diretório estranho, execute-o diretamente da pasta src")
+
+  if 'data' in dir:
+    return "{}data/".format(dir_string)
+  else:
+    return set_data_folder("{}../".format(dir_string), tries_count + 1)
+
+
+DATA_FOLDER = set_data_folder()
+
+def get_file(filename):
+  return open("{}{}".format(DATA_FOLDER, filename), "r")
+
+@cached
+def get_gastos_file(ano):
+  return get_file("gastos-{}.csv".format(ano))
